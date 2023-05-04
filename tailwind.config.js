53a86a7793791fc0b383bbf929ad706976f3c3c6
Andy Stabler <me@andystabler.co.uk>
2023-05-04 22:32:07 +0100

    Add a homepage

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./source/**/*.{html,erb}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter'],
+      }
+    },
+  },
+  plugins: [],
+}
